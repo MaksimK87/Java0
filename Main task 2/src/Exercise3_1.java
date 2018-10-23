91985d06501a11d298b78319ec2bae0e1c082c31
MaksimK87 <max87kosm@gmail.com>
2018-10-23 23:15:42 +0300

    Home task

--- Main task 2/src/Exercise3_1.java
+++ Main task 2/src/Exercise3_1.java
@@ -7,13 +7,25 @@ import java.util.Scanner;
 public class Exercise3_1 {
 
 	public static void main(String[] args) {
-		int n, heads = 0, i = 0;
-		char simvol;
+		String simvol;
+		int index=0;
+		char letter;
 		char [] vowels= {'a','e','i','o','u','y','A','E','I','O','U','Y'};
 		Scanner in = new Scanner(System.in);
-		System.out.println("¬ведите гласную на латинице: ");
-		simvol = in.next;
-
+		System.out.println("¬ведите букву на латинице: ");
+		simvol = in.nextLine();
+		letter=simvol.charAt(0);
+		for(int i=0;i<vowels.length;i++) {
+			if(letter==vowels[i]) {
+			 index=i;
+			}
+			if(letter==vowels[index])
+				System.out.println("¬веденный символ €вл€етс€ гласной");
+			else 
+				System.out.println("¬веденный символ не €вл€етс€ гласной");
+		}
+		
+		
 	}
 
 }
